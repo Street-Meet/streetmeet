93b0a741b7b3266c01ae8fac5fb5c474ac415119
xelad1 <xelad1@github.com>
2014-12-17 17:25:15 -0800

    Added routing for login, createEvent, and joinEvent

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,22 @@
 var meetApp = angular.module('sm-meetApp',
     [
-        'ui.router'
-    ]);
+        'ui.router';
+    ])
+        .config(function($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state('login', {
+                    url: 'login',
+                    template: 'views/login.html',
+                    controller: ''
+                })
+                .state('createEvent', {
+                    url: 'createEvent',
+                    template: 'views/createEvent.html',
+                    controller: ''
+                })
+                .state('joinEvent', {
+                    url: 'joinEvent',
+                    template: 'views/joinEvent.html',
+                    controller: ''
+                })
+        });
