dab9d1163b0436254930a5640c272650734edf70
Brett Leibowitz <LEIBO@Bretts-MacBook-Pro.local>
2014-12-18 10:43:12 -0800

    rebasing to add ionic

---
+++ streetMeet/www/js/app.js
@@ -0,0 +1,47 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var meetApp = angular.module('sm-meetApp',
+  [
+    'ionic', 'google-maps'
+  ])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+.controller('MapController', function($scope, $ionicLoading) {
+
+  google.maps.event.addDomListener(window, 'load', function() {
+    var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      var myLocation = new google.maps.Marker({
+        position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+        map: map,
+        title: "My Location"
+      });
+    });
+
+    $scope.map = map;
+  });
+});
