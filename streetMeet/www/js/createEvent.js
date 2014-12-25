8419767f68234396a10b54073ab7cd73125bf6c1
Brett Leibowitz <LEIBO@bretts-mbp.attlocal.net>
2014-12-25 12:19:01 -0600

    fixed spaces

--- streetMeet/www/js/createEvent.js
+++ streetMeet/www/js/createEvent.js
@@ -5,31 +5,31 @@ angular.module('sm-meetApp.createEvents',  ["firebase", 'ngCookies'])
 
    $scope.createEvent = function(eventTitle, eventDescription, eventCapacity, eventCategory, eventMinCapacity){
     Events.createEvent($cookieStore.get('currentUser'), eventTitle, eventDescription, eventCapacity, eventCategory, eventMinCapacity);
-  } 
+  }
 
 
 })
 .factory('Events', function ($q) {
-    var ref = new Firebase("https://boiling-torch-2747.firebaseio.com/");
-    var createEvent = function(owner ,eventTitle, eventDescription, eventCapacity, eventCategory, eventMinCapacity){
-        var eventData ={
-            user_id: owner,
-            title: eventTitle,
-            description: eventDescription,
-            capacity: eventCapacity,
-            category: eventCategory,
-            min_capacity: eventMinCapacity
-        };
-        var eventsRef = ref.child("events");
-        eventsRef.push(eventData, function(error) {
-          if (error) {
-            alert("Data could not be saved." + error);
-          } else {
-            alert("Data saved successfully.");
-          }
-        });
+  var ref = new Firebase("https://boiling-torch-2747.firebaseio.com/");
+  var createEvent = function(owner ,eventTitle, eventDescription, eventCapacity, eventCategory, eventMinCapacity){
+    var eventData ={
+      user_id: owner,
+      title: eventTitle,
+      description: eventDescription,
+      capacity: eventCapacity,
+      category: eventCategory,
+      min_capacity: eventMinCapacity
     };
-    return{
-       createEvent: createEvent
-    };
+    var eventsRef = ref.child("events");
+    eventsRef.push(eventData, function(error) {
+      if (error) {
+        alert("Data could not be saved." + error);
+      } else {
+        alert("Data saved successfully.");
+      }
+    });
+  };
+  return{
+     createEvent: createEvent
+  };
  });
--- streetMeet/www/js/joinEvent.js
+++ streetMeet/www/js/joinEvent.js
@@ -1,14 +1,14 @@
 angular.module('sm-meetApp.joinEvent',  ["firebase"])
 
 .controller('JoinEventCtrl', ["$scope", "$firebase", function($scope, $firebase) {
-    
 
+  var ref = new Firebase("https://boiling-torch-2747.firebaseio.com/events");
 
-    var ref = new Firebase("https://boiling-torch-2747.firebaseio.com/events");
-    var sync = $firebase(ref);
+  var ref = new Firebase("https://boiling-torch-2747.firebaseio.com/events");
+  var sync = $firebase(ref);
 
-    // if ref points to a data collection
-    $scope.list = sync.$asArray();
-    // $scope.events = syncObject;
+  // if ref points to a data collection
+  $scope.list = sync.$asArray();
+  // $scope.events = syncObject;
 
 }]);
