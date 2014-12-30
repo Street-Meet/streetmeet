aea1851dbe896b316d9629a37b40e63ed97f088a
Brett Leibowitz <LEIBO@Bretts-MacBook-Pro.local>
2014-12-29 19:43:51 -0800

    removed extra line

--- streetMeet/www/js/joinEvent.js
+++ streetMeet/www/js/joinEvent.js
@@ -4,11 +4,9 @@ angular.module('sm-meetApp.joinEvent',  ["firebase"])
 
   var ref = new Firebase("https://boiling-torch-2747.firebaseio.com/events");
 
-  var ref = new Firebase("https://boiling-torch-2747.firebaseio.com/events");
   var sync = $firebase(ref);
 
   // if ref points to a data collection
   $scope.list = sync.$asArray();
   // $scope.events = syncObject;
-
 }]);
