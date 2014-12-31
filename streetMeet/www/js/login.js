fabc479a7f4d218f9fa11a00ae8e6942ef66666e
Rene Polo <renejpolo@me.com>
2014-12-30 23:28:15 -0800

    user events added, working wonderful

--- streetMeet/www/js/login.js
+++ streetMeet/www/js/login.js
@@ -2,8 +2,8 @@ angular.module('sm-meetApp.login',  ['firebase', 'ngCookies'])
 
 .controller('LoginCtrl', ["$scope",  "$firebaseAuth", "$cookieStore", 
   function($scope, $firebaseAuth, $cookieStore) {
-    $scope.currentUser;
-    $scope.currentUserId;
+    $scope.currentUser =  $cookieStore.get('currentData') || null;
+    $scope.currentUserId =  $cookieStore.get('currentUser') || null; 
     $scope.theEvents;
     var ref = new Firebase("https://boiling-torch-2747.firebaseio.com/");
     var auth = $firebaseAuth(ref);
@@ -27,6 +27,8 @@ angular.module('sm-meetApp.login',  ['firebase', 'ngCookies'])
       console.log('this is the authData: ', authData);
         $cookieStore.put('currentUser', authData.uid );
         $cookieStore.put('currentToken', authData.token );
+        $cookieStore.put('currentData', authData.facebook.cachedUserProfile );
+        
         console.log("Logged in as:", authData.uid);
         $scope.currentUser = authData.facebook.cachedUserProfile;
         $scope.currentUserId = authData;
@@ -57,15 +59,24 @@ angular.module('sm-meetApp.login',  ['firebase', 'ngCookies'])
       var eventsRef = new Firebase("https://boiling-torch-2747.firebaseio.com/events");
       var userRef =   new Firebase("https://boiling-torch-2747.firebaseio.com/users");
       var userEventsRef = userRef.child(user_id).child("events");
+      var result = [];
       userEventsRef.on("child_added", function(snap) {
         console.log('this is the snap:', snap.val());
-        eventsRef.child(snap.key()).once("value", function(data) {
-          console.log('YourEvents ',  data.val());
-          $scope.theEvents = data.val();
+      eventsRef.child(snap.key()).on("value", function(data) {
+          result.push(data.val());
+          
+          
         })
+      $scope.theEvents = result;
       });
      
   };
+  $scope.logout = function(){
+      auth.$unauth();
+      $cookieStore.remove('currentData')
+      $cookieStore.remove('currentUser')
+      $cookieStore.remove('currentToken');
+  }
 }]);
   //TODO: CurrentUser as a service for every other view who wants to use it.
 
