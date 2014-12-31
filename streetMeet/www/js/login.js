7ce20473089b1975c280d41f6994fe5275ad4386
Rene Polo <renejpolo@me.com>
2014-12-30 19:06:40 -0800

    events as a service

--- streetMeet/www/js/login.js
+++ streetMeet/www/js/login.js
@@ -7,7 +7,6 @@ angular.module('sm-meetApp.login',  ['firebase', 'ngCookies'])
     var auth = $firebaseAuth(ref);
 
     $scope.simpleLogin = function(theEmail, thePass){
-      console.log('clicked Simple login');
       auth.$authWithPassword({
         email: theEmail,
         password: thePass
@@ -17,23 +16,52 @@ angular.module('sm-meetApp.login',  ['firebase', 'ngCookies'])
       }).catch(function(error) {
         console.error("Authentication failed:", error);
       });
-    }
+    };
 
     $scope.loginWithFacebook = function(){
-    console.log('clicked Facebook login');
     auth.$authWithOAuthPopup("facebook",
       {scope: "email, user_events" }) // scope has the permissions requested
     .then(function(authData) {
       console.log('this is the authData: ', authData);
-      $cookieStore.put('currentUser', authData.uid );
-      $cookieStore.put('currentToken', authData.token );
+        $cookieStore.put('currentUser', authData.uid );
+        $cookieStore.put('currentToken', authData.token );
         console.log("Logged in as:", authData.uid);
         $scope.currentUser = authData.facebook.cachedUserProfile;
       }).catch(function(error) {
         console.error("Authentication failed:", error);
       });
     };
-  }
+
+
+    $scope.getSpecificEvent = function(event_id){
+     //location for any given event
+      
+      var locationRef = new Firebase("https://boiling-torch-2747.firebaseio.com/locations");
+      var eventRef =  new Firebase("https://boiling-torch-2747.firebaseio.com/events");
+      var eventLocationRef = eventRef.child(event_id).child("locations")  ;
+     
+
+      eventLocationRef.on("child_added", function(snap) {
+        locationRef.child(snap.key()).once("value", function(){
+          // Render the location on the events page.
+        }) 
+      });
+      };
+
+    $scope.getMyEvents = function(user_id){
+        //events for any given user
+      var eventsRef = new Firebase("https://boiling-torch-2747.firebaseio.com/events");
+      var userRef =  new Firebase("https://boiling-torch-2747.firebaseio.com/users");
+      var userEventsRef = userRef.child(user_id).child("events");
+      userEventsRef.on("child_added", function(snap) {
+        eventsRef.child(snap.key()).once("value", function() {
+          // Render the event on the user page.
+        })
+      });
+     
+  };
+}
+
 ]);
 
 
