e24a7b0961de6f6dc0ec451f1afad217e85e49fb
Brett Leibowitz <LEIBO@Bretts-MacBook-Pro.local>
2015-01-07 11:37:29 -0800

    moved joinEvent to factory

--- streetMeet/www/js/event.js
+++ streetMeet/www/js/event.js
@@ -1,27 +1,27 @@
 angular.module('sm-meetApp.event',  ["firebase", 'ngCookies'])
 
-.controller('EventCtrl', function($scope, $firebase, $cookieStore, $state) {
-  // angular.extend($scope, Event);
+.controller('EventCtrl', function($scope, $firebase, $cookieStore, $state, Event) {
+  angular.extend($scope, Event);
   var refEvent = new Firebase("https://boiling-torch-2747.firebaseio.com/current/events/"+$state.params.id);
   refEvent.on('value', function(snap) {
     // check to see if is a valid key, otherwise reroute
     $scope.eventData = snap.val();
     console.log($scope.eventData);
   })
+})
 
-  $scope.joinEvent = function() {
+.factory('Event', function ($q, $cookieStore, $state) {
+  var joinEvent = function() {
     var ref = new Firebase("https://boiling-torch-2747.firebaseio.com/current/events/"+$state.params.id+"/attendees/"+$cookieStore.get('currentUser'));
     ref.set(true, function(error) {
-        if (error) {
-          alert("Data could not be saved." + error);
-        } else {
-          console.log("Attendee data saved successfully.");
-        }
-      });
+      if (error) {
+        alert("Data could not be saved." + error);
+      } else {
+        console.log("Attendee data saved successfully.");
+      }
+    });
   }
-});
-
-// .factory('Event', function ($q, $cookieStore, $state) {
-//   return{
-//   };
-//  });
+  return{
+    joinEvent : joinEvent
+  };
+ });
