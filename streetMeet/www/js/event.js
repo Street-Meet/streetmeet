a79b014708d819234281b44d69383c5a7138f6ee
Brett Leibowitz <LEIBO@Bretts-MacBook-Pro.local>
2015-01-09 12:28:55 -0800

    attendee list updates on join

--- streetMeet/www/js/event.js
+++ streetMeet/www/js/event.js
@@ -24,23 +24,24 @@ angular.module('sm-meetApp.event',  ["firebase", 'ngCookies'])
   var id = ref.child("/users/");
 
   // list of attendees
-  var attendeeObj = $firebase(refAttendees).$asObject();
-  attendeeObj.$loaded().then(function() {
-    console.log("loaded record:", attendeeObj.$id);
-    angular.forEach(attendeeObj, function(value, key) {
-      var userObj = $firebase(ref.child("/users/"+key+"/userInfo")).$asObject();
-      // grab user info to later display for each attendee
-      userObj.$loaded().then(function() {
-        result[key] = userObj;
-        $scope.attendees = result;
-      })
+  // $scope.attendees = Event.attendList();
+
+  $scope.update = function() {
+    var attendeeObj = $firebase(refAttendees).$asObject();
+    attendeeObj.$loaded().then(function() {
+      console.log("loaded record:", attendeeObj.$id);
+      angular.forEach(attendeeObj, function(value, key) {
+        var userObj = $firebase(ref.child("/users/"+key+"/userInfo")).$asObject();
+        // grab user info to later display for each attendee
+        userObj.$loaded().then(function() {
+          result[key] = userObj;
+          $scope.attendees = result;
+        });
+      });
     });
-  });
-
-})
-
-.factory('Event', function ($q, $cookieStore, $state) {
-  var joinEvent = function() {
+  }
+  $scope.update();
+  $scope.joinEvent =function() {
     var ref = new Firebase("https://boiling-torch-2747.firebaseio.com/current/events/"+$state.params.id+"/attendees/"+$cookieStore.get('currentUser'));
     var userRef = new Firebase("https://boiling-torch-2747.firebaseio.com/users/"+$cookieStore.get('currentUser'));
     ref.set(true, function(error) {
@@ -48,6 +49,7 @@ angular.module('sm-meetApp.event',  ["firebase", 'ngCookies'])
         alert("Data could not be saved." + error);
       } else {
         console.log("Attendee data saved successfully.");
+        $scope.update();
       }
     });
     userRef.child("/currentEvent/").set($state.params.id, function(error) {
@@ -66,7 +68,9 @@ angular.module('sm-meetApp.event',  ["firebase", 'ngCookies'])
     });
   }
 
+})
+
+.factory('Event', function ($q, $cookieStore, $state) {
   return{
-    joinEvent : joinEvent
   };
  });
