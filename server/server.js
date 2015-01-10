24988c1030491070ade6a4ccf16cd148900d5098
Rene Polo <renejpolo@me.com>
2015-01-10 00:20:22 -0800

    facebook working on mobile

--- server/server.js
+++ server/server.js
@@ -1,103 +1,82 @@
 var express = require('express');
 var Meetup = require('meetup');
 var url = require('url');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var request = require('request');
 var cors = require('cors');
 var app = express();
 var path = require('path');
+var  _ = require('underscore');
+// var methodOverride = require('method-override');
 
-// var graph     = Promise.promisifyAll(require('fbgraph'));
-// var Promise = require("bluebird");
-// var _ = require('underscore');
-
-
-// var mup = new Meetup({
-//   clientId:"3fss73ueag5e744sp45cmopag2",
-//   clientSecret:"g31i2o39pbf2qgscie95pkcdod",
-//   redirectUri:"http://127.0.0.1:3000"
-// });
-
-// var corsOptions = {
-//   origin: 'http://localhost:3000'
-// };
-
-// app.use(bodyParser.json());
-// app.use(cors(corsOptions));
-
-// app.get('/login/meetup', function(req, res){
-//    var meetupUrl = mup.getAuthorizeUrl();
-
-//     res.redirect(meetupUrl);
-//     res.end();
-// });
-
-// app.get('/*', function(req, res, next){
-//     var url_parts = url.parse(req.url, true);
-//     var query = url_parts.query;
-//     var code = query.code;
-//     if (!code) {
-//       next();
-//     } else {
-//       res.sendFile(path.resolve(__dirname+'../../streetMeet/www/index.html'));
-//     }
-    
-// });
-
-// app.get('/api/meetup/self', function(){
-   
-// });
-
-// app.get('/api/facebook/:token', function(req, res2){
-//   console.log('llego a getthefeed');
-//   var count = 0;
-//   var theResult = [];
-//   var  _theToken = req.params.token
-//   var  fbToken = req.params.token
-
-//   console.log('this is the token: '+ _theToken);
-//   var getFacebookGraphData = function(theData,  _theToken){
-//     theData = theData || 'me';
-//     _theToken = _theToken || {access_token: fbToken};
-
-//     graph.getAsync(theData, _theToken)
-//     .then(function(res){
-//       console.log('llego dentro del then');
-//       if(count<1){
-//         count++;
-// // console.log('OTRO DATA SET ----------------------------- ' , res.data);
-// theResult.push(res.data);
-// // console.log('vuelta numero: '+ count + 'resultado en ' + res.paging.next);
-// if(res.paging && res.paging.next) {
-//   getFacebookGraphData(res.paging.next, null);
-// }
-// }
-// return theResult;
-// })
-//     .then(function(results){
-// // eventEmitter.emit('pageLoaded', res2, results);
-// // console.log(results)
-// console.log('EL COUNT FUE DE ' + count);
-// if(count === 1){
-// // res2.render('feed', { res: _.flatten(results) });
-// res2.json(_.flatten(results))
-// }
-// })
-//     .catch(function(e){
-//       console.error("Error: ", e);
-//     });
-//   };
-
-//   console.log('llamando a la funcion');
-//   getFacebookGraphData();
-// });
+// app.use(bodyParser());          // pull information from html in POST
+// app.use(methodOverride());      // simulate DELETE and PUT
+app.use(cors());
 
 var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '../../streetMeet/www'));
 
+var mup = new Meetup({
+  clientId:"3fss73ueag5e744sp45cmopag2",
+  clientSecret:"g31i2o39pbf2qgscie95pkcdod",
+  redirectUri:"http://localhost/callback"
+});
 
+
+app.get('/api/meetup/:lat/:long/', function(req, res){
+console.log('inside get server');
+    var reqLat =  req.params.lat;
+    var reqLong = req.params.long;
+    if(req.param("token")){
+       mup.options.accessToken = req.param("token"); 
+    }else{
+        console.log('redoing the query');
+    }
+    
+    var result= [];
+    //data streaming from Meetup
+        // mup.stream("/2/open_events?&sign=true&photo-host=public&zip=94102&page=20", function(stream){
+        //   stream
+        //     .on("data", function(item){
+        //       console.log("got item ", item)
+        //       result.push(item);
+        //       if(result.length>5){
+        //       res.json(_.flatten(result));
+        //       stream.destroy()
+        //     }
+              
+        //     }).on("error", function(e) {
+        //        console.log("error! " + e)
+        //     });
+        // });  
+    var actualDate = new Date();
+    var dateInMs = actualDate.getTime();
+    var desiredTime = 1320000
+    var oneDay = 86400000; 
+    var oneHour = 3600000;
+    var twentytwoMins = dateInMs+oneDay;
+
+    meetupQuery  = "/2/open_events?lon="+reqLong.toString()+"&lat="+reqLat.toString()+"&radius=1&time="+dateInMs.toString()+","+twentytwoMins.toString()+"&sign=true&photo-host=public";
+    console.log('applying this query: '+ meetupQuery);
+    mup.get(meetupQuery, function(err, data){
+      if(!err){
+      // console.log("got data thru GET: " , JSON.parse(data));
+      res.json(JSON.parse(data));
+
+    }else{
+        console.log('got err: ', err);
+    }
+    })
+
+});
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 
 
 app.listen(port);
-
 
