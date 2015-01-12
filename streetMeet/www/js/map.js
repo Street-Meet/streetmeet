695c4ec9f0de73cab639381305126fd01aca76e8
xelad1 <xelad1@github.com>
2015-01-12 14:55:42 -0800

    Map now goes to entered location

--- streetMeet/www/js/map.js
+++ streetMeet/www/js/map.js
@@ -26,22 +26,66 @@ angular.module('sm-meetApp.map',  ['firebase'])
     center: center,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
+
+  /*Geocoding and address autofill*/
   var input = /** @type {HTMLInputElement} */(
       document.getElementById('pac-input'));
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   
   var autocomplete = new google.maps.places.Autocomplete(input);
-  // autocomplete.bindTo('bounds', map);
-  // var initialize = function() {
-    var center = new google.maps.LatLng(47.785326, -122.405696);
-    var globalLatLng;
+  autocomplete.bindTo('bounds', map);
+  var infowindow = new google.maps.InfoWindow();
+  
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      return;
+    }
 
-    var mapOptions = {
-      zoom: 15,
-      center: center,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);  // Why 17? Because it looks good.
+    }
+    marker.setIcon(/** @type {google.maps.Icon} */({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+  });
+
+
+
+  // var initialize = function() {
+    // var center = new google.maps.LatLng(47.785326, -122.405696);
+    // var globalLatLng;
+
+    // var mapOptions = {
+    //   zoom: 15,
+    //   center: center,
+    //   mapTypeId: google.maps.MapTypeId.ROADMAP
+    // };
+    // var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     // return map;
   // }
 
