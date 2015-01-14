bd85209326c98a186d45e0fd4c428d48a2331a71
Brett Leibowitz <LEIBO@Bretts-MacBook-Pro.local>
2015-01-13 21:35:36 -0800

    profile settings editing beginning to pull in data

---
+++ streetMeet/www/js/profileSettings.js
@@ -0,0 +1,27 @@
+angular.module('sm-meetApp.profileSettings',  ["firebase"])
+
+.controller('ProfileSettingsCtrl', function($scope, $firebase, $cookieStore, $state, $q) {
+  var refUser = new Firebase("https://boiling-torch-2747.firebaseio.com/users/"+$state.params.id+"/userInfo");
+  var userSync = $firebase(refUser);
+  var userObj = userSync.$asObject();
+  userObj.$loaded().then(function() {
+    console.log(userObj);
+    userObj.$bindTo($scope, "userData").then(function() {
+      console.log($scope.userData);
+    });
+  }).then(function() {
+    $scope.name = $scope.userData.first_name + ' ' + $scope.userData.last_name;
+    $scope.email = $scope.userData.email;
+    // $scope.eventCapacity = $scope.eventData.capacity;
+    // $scope.eventAddress = $scope.eventData.address;
+  });
+
+  $scope.save = function(permanent, edit) {
+    // if (permanent === 'name') {
+    //   $scope.userData[permanent] = edit;
+    // } else {
+
+    // }
+  }
+
+});
