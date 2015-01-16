19b2bd1ef8f293b77c15aebb3d284c95435c505c
Brett Leibowitz <LEIBO@Bretts-MacBook-Pro.local>
2015-01-16 01:31:50 -0800

    testing with leaveEvent. possibly some bugs still

--- streetMeet/www/js/event.js
+++ streetMeet/www/js/event.js
@@ -144,73 +144,8 @@ angular.module('sm-meetApp.event',  ["firebase", 'ngCookies'])
         });
       });
     }
+  })
 
-  // // user leaves an event
-  // $scope.leaveEvent =function() {
-  //   var ownerRef = new Firebase("https://boiling-torch-2747.firebaseio.com/events/"+$state.params.id +"/owner");
-  //   var ownerSync = $firebase(ownerRef);
-  //   ownerObj = ownerSync.$asObject();
-  //   ownerObj.$loaded().then(function() {
-  //     $q(function(resolve, reject) {
-  //       var ref = new Firebase("https://boiling-torch-2747.firebaseio.com/events/"+$state.params.id+"/attendees/"+$cookieStore.get('currentUser'));
-  //       var userRef = new Firebase("https://boiling-torch-2747.firebaseio.com/users/"+$cookieStore.get('currentUser'));
-  //       // marks user as left in attendee list
-  //       ref.set(false, function(error) {
-  //         if (error) {
-  //           alert("Data could not be saved." + error);
-  //           reject('rejected');
-  //         } else {
-  //           userRef.child("/currentEvent/").remove();
-  //           // $scope.update();
-  //           resolve('resolved');
-  //         }
-  //       });
-  //     })
-  //     .then(function() {
-  //       angular.forEach(ownerObj, function (value, key) {
-  //         if (key === $cookieStore.get('currentUser') && value === true) {
-  //           ownerRef.child(key).set(false, function(error) {
-  //             if (error) {
-  //               alert("Data could not be saved." + error);
-  //             } else {
-  //               $q(function(resolve, reject) {
-  //                 $scope.update();
-  //                 resolve();
-  //               }).then(function() {
-  //                 $state.transitionTo('map', {
-  //                   reload: true,
-  //                   inherit: false,
-  //                   notify: false
-  //                 });
-  //               })
-  //             }
-  //           });
-  //         } else {
-  //           $q(function(resolve, reject) {
-  //             $scope.update();
-  //             resolve();
-  //           }).then(function() {
-
-  //             $state.transitionTo('map', {
-  //               reload: true,
-  //               inherit: false,
-  //               notify: false
-  //             });
-  //           })
-  //             console.log('transitioning');
-  //             transitionToMap();
-  //             console.log("Owner data saved successfully.");
-  //             console.log('in promise');
-  //           }
-  //           });
-  //         });
-  //       });
-  //     });
-
-
-    })
-//   }
-// })
 
 .factory('Event', function ($q, $cookieStore, $state, $firebase) {
   return {
